e81782ab37f4b0d79048831b7a2e070244cbfb42
kawaribeiro2310 <kawaribeiroalves@gmail.com>
2026-08-03 19:33:17 -0300

    Aprendi diferente tripos de variaveis tipos de imprimir no console seja ele para input ou só imprimir o resultado e os tipos de class pelo print

---
+++ U1_A1/Criando variaveis de tipos diferentes.py
@@ -0,0 +1,16 @@
+x = 10
+nome = "KAWA"
+nota = 9.5
+inscrito = True
+
+print (nome)
+print (nota)
+print (inscrito)
+
+print(type(x))
+print(type(nome))
+print(type(nota))
+print(type(inscrito))
+
+nome2 = input("Digite seu nome: ")
+print (f"Seja bem-vindo(a), {nome2}!")
