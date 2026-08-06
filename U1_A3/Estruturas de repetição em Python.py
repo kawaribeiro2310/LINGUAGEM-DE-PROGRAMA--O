0b04a5d1b06963631525063e90b3ed0cac3ddb3b
kawaribeiro2310 <kawaribeiroalves@gmail.com>
2026-08-06 20:40:22 -0300

    interpretando como funciona as funçao de repetição

---
+++ U1_A3/Estruturas de repetição em Python.py
@@ -0,0 +1,9 @@
+
+numero = int(input("Digite um número: "))
+
+while numero != 0:
+    if numero % 2 == 0:
+        print(f"{numero} é par")
+    else:
+        print(f"{numero} é ímpar") 
+        break
