d68f48dcf7e00f27a550de7067ea8d373152c10b
kawaribeiro2310 <kawaribeiroalves@gmail.com>
2026-08-10 19:24:28 -0300

    vou dormir finalizei por hj

--- U2_A1/aula3.py
+++ U2_A1/aula3.py
@@ -4,13 +4,14 @@ class Pessoa:
         self.idade = idade
         self.genero = genero
 
-def comprimentar(self):
-    return f"Olá, meu nome é {self.nome} e tenho {self.idade} anos."
+    def comprimentar(self):
+        return f"Olá, meu nome é {self.nome} e tenho {self.idade} anos."
 
-def aniversario(self):
-    self.idade += 1
-    return f"Feliz aniversário, {self.nome}! Agora você tem {self.idade} anos."
+    def aniversario(self):
+        self.idade += 1
+        return f"Feliz aniversário, {self.nome}! Agora você tem {self.idade} anos."
 
-pessoa1 = Pessoa("João", 25)
+pessoa1 = Pessoa("João", 25, "Masculino")
+
 print(pessoa1.comprimentar())  # Saída: Olá, meu nome é João e tenho 25 anos.
 print(pessoa1.aniversario())  # Saída: Feliz aniversário, João! Agora você tem 26 anos. 
