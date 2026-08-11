521635bb294ec11f099be9bb1f5a34503f77136b
kawaribeiro2310 <kawaribeiroalves@gmail.com>
2026-08-11 19:05:00 -0300

    Vi as 2 duas formas de importar uma biblioteca

--- U2_A2/Bibliotecas e módulos em Python.py
+++ U2_A2/Bibliotecas e módulos em Python.py
@@ -3,3 +3,17 @@ import math
 print("Raiz quadrada de 16:", math.sqrt(16))
 print("Valor de pi:", math.pi)
 print("Fatorial de 5:", math.factorial(5))
+
+# Segunda forma de importar uma biblioteca
+
+import math as m
+print("Raiz quadrada de 25:", m.sqrt(25))
+print("Valor de pi:", m.pi)
+print("Fatorial de 5:", m.factorial(5))
+
+#Terceira forma de importar uma biblioteca
+from math import sqrt, pi, factorial
+
+print("Raiz quadrada de 36:", sqrt(36))
+print("Valor de pi:", pi)
+print("Fatorial de 6:", factorial(6))
